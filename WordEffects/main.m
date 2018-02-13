a283b34b1ed75d39bec9fb0ac0f15fddbc74b712
KevinT <kevin.tai.mail@gmail.com>
2018-02-12 18:51:53 -0800

    stops to input number

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -11,18 +11,43 @@
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     while (TRUE) {
-      // 255 unit long array of characters
+      
+      char inputNumber[255];
+      
+      printf("Input a number 1-6: ");
+      
+      fgets(inputNumber, 255, stdin);
+      
       char inputChars[255];
       
       printf("Input a string: ");
       // limit input to max 255 characters
       fgets(inputChars, 255, stdin);
       
+      NSString *inputString = [[NSString alloc] initWithCString:inputChars encoding:NSUTF8StringEncoding];
+      
+      if (inputNumber[0] == '1') {
+        NSLog(@"You entered the number 1 and string %s.", inputChars);
+      } else if (inputNumber[0] == '2') {
+        NSLog(@"You entered the number 2 and string %s.", inputChars);
+      } else if (inputNumber[0] == '3') {
+        NSLog(@"You entered the number 3 and string %s.", inputChars);
+      } else if (inputNumber[0] == '4') {
+        NSLog(@"You entered the number 4 and string %s.", inputChars);
+      } else if (inputNumber[0] == '5') {
+        NSLog(@"You entered the number 5 and string %s.", inputChars);
+      } else if (inputNumber[0] == '6') {
+        NSLog(@"You entered the number 6 and string %s.", inputChars);
+      } else {
+        NSLog(@"enter a number 1-6");
+      }
+      
+      
       // print as a c string
-      printf("Your string is %s\n", inputChars);
+      //printf("Your string is %s\n", inputChars);
       
       // convert char array to an NSString object
-      NSString *inputString = [NSString stringWithUTF8String:inputChars];
+      //NSString *inputString = [NSString stringWithUTF8String:inputChars];
       
       // print NSString object
       NSLog(@"Input was: %@", inputString);
